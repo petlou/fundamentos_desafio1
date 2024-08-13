6ca2edc04ade5c5b44ce1764dfcb41c570eadc3a
Peter Lourenço <peter.lourenco@brisabr.com.br>
2024-08-12 22:53:43 -0300

    feat: Primeiro desafio

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿namespace fundamentos_desafio;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Olá! Por favor, digite seu nome:");
+        string? name = Console.ReadLine();
+        Console.Clear();
+
+        Console.WriteLine($"Olá, {name}! Seja muito bem - vindo!");
+    }
+}
