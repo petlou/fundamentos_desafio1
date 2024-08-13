ef2b693be27294d14e01776614b74e73cb0c06d1
Peter Lourenço <peter.lourenco@brisabr.com.br>
2024-08-12 23:29:27 -0300

    feat: Exercício 2

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
-﻿namespace fundamentos_desafio;
+﻿using fundamentos_desafio.Task1;
+
+namespace fundamentos_desafio;
 
 class Program
 {
@@ -9,5 +11,46 @@ class Program
         Console.Clear();
 
         Console.WriteLine($"Olá, {name}! Seja muito bem - vindo!");
+        Console.WriteLine();
+        Console.WriteLine("----------------------");
+        Console.WriteLine();
+        Console.WriteLine("Para continuar, pressione ENTER");
+        Console.ReadLine();
+        Console.Clear();
+
+        int numberSelected;
+
+        do
+        {
+            Console.Clear();
+
+            Console.WriteLine("Escolha abaixo o que deseja fazer:");
+            Console.WriteLine();
+            Console.WriteLine("1 - Concatenar nome e sobrenome");
+            Console.WriteLine("0 - Sair");
+
+            numberSelected = int.Parse(Console.ReadLine());
+            Console.WriteLine(numberSelected);
+
+            switch (numberSelected)
+            {
+                case 0:
+                    break;
+                case 1:
+                    var concat = new ConcatName();
+                    concat.Concat();
+                    break;
+                default:
+                    Console.Clear();
+                    Console.WriteLine($"A opção escolhida não é válida: {numberSelected}");
+                    Console.WriteLine("Para continuar, pressione ENTER");
+                    Console.ReadLine();
+                    break;
+            }
+        }
+        while (numberSelected > 0);
+
+        Console.Clear();
+        Console.WriteLine("O Programa foi encerrado!");
     }
 }
---
+++ Task1/ConcatName.cs
@@ -0,0 +1,22 @@
+﻿namespace fundamentos_desafio.Task1;
+public class ConcatName
+{
+    public void Concat()
+    {
+        Console.Clear();
+
+        Console.WriteLine("Por favor, digite o primeiro nome:");
+        string? name = Console.ReadLine();
+        Console.Clear();
+
+        Console.WriteLine("Por favor, digite o sobrenome:");
+        string? lastName = Console.ReadLine();
+        Console.Clear();
+
+        Console.WriteLine($"{name} {lastName}");
+        Console.WriteLine();
+        Console.WriteLine("Presione ENTER para voltar para o Menu anterior");
+        Console.ReadLine();
+        Console.Clear();
+    }
+}
